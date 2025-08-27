558d0105d11c0927c1c0f1e102d66c2b014d87fc
lanyangyg <fnnnfnnfn@126.com>
2025-08-27 12:07:29 +0800

    SeleniumManager update chromedriverVersion

---
+++ common/SeleniumManager_update_chromedriverVersion.py
@@ -0,0 +1,11 @@
+# SeleniumManager update chromedriverVersion (需要vpn)
+
+# import os
+# from selenium import webdriver
+#
+# # 设置代理（示例为本地 Clash/V2Ray，端口请改成自己的）
+# os.environ["HTTPS_PROXY"] = "http://127.0.0.1:1087"
+#
+# driver = webdriver.Chrome()
+# print(driver.capabilities['chrome']['chromedriverVersion'])
+# driver.quit()
